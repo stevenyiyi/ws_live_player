66670655c88a46efe85f94fe060fa634f2a0e3f0
stevenyiyi <104037806@qq.com>
2023-07-21 17:03:15 +0800

    zfasfa

--- src/ASWorkSupport.js
+++ src/ASWorkSupport.js
@@ -1,135 +1,148 @@
 /* global ArrayBuffer */
 
-import ASLoader from './ASLoaderWorker.js';
-
+import ASLoader from "./ASLoaderWorker.js";
+// Re-export ASLoader so the base URL can be inserted
+// when using CDN compatibility blob loading.
+global.ASLoader = ASLoader;
 /**
  * Web Worker wrapper for codec fun
  */
 class ASWorkerSupport {
-	constructor(propList, handlers) {
-		this.propList = propList;
-		this.handlers = handlers;
+  constructor(propList, handlers) {
+    this.propList = propList;
+    this.handlers = handlers;
 
-		this.transferables = (() => {
-			let buffer = new ArrayBuffer(1024),
-				bytes = new Uint8Array(buffer);
-			try {
-				postMessage({
-					action: 'transferTest',
-					bytes: bytes
-				}, [buffer]);
-				if (buffer.byteLength) {
-					// No transferable support
-					return false;
-				} else {
-					return true;
-				}
-			} catch (e) {
-				return false;
-			}
-		})();
+    this.transferables = (() => {
+      let buffer = new ArrayBuffer(1024),
+        bytes = new Uint8Array(buffer);
+      try {
+        postMessage(
+          {
+            action: "transferTest",
+            bytes: bytes
+          },
+          [buffer]
+        );
+        if (buffer.byteLength) {
+          // No transferable support
+          return false;
+        } else {
+          return true;
+        }
+      } catch (e) {
+        return false;
+      }
+    })();
 
-		this.target = null;
+    this.target = null;
 
-		this.sentProps = {};
-		this.pendingEvents = [];
+    this.sentProps = {};
+    this.pendingEvents = [];
 
-		this.handlers.construct = (args, callback) => {
-			let className = args[0],
-				options = args[1];
+    this.handlers.construct = (args, callback) => {
+      let className = args[0],
+        options = args[1];
 
-			ASLoader.loadClass(className, (classObj) => {
-				classObj(options).then((target) => {
-					this.target = target;
-					callback();
-					while (this.pendingEvents.length) {
-						this.handleEvent(this.pendingEvents.shift());
-					}
-				});
-			});
-		};
+      ASLoader.loadClass(className, (classObj) => {
+        classObj(options).then((target) => {
+          this.target = target;
+          callback();
+          while (this.pendingEvents.length) {
+            this.handleEvent(this.pendingEvents.shift());
+          }
+        });
+      });
+    };
 
-		addEventListener('message', (event) => {
-			this.workerOnMessage(event);
-		});
-	}
+    this.addEventListener("message", (event) => {
+      this.workerOnMessage(event);
+    });
+  }
 
-	handleEvent(data) {
-		this.handlers[data.action].call(this, data.args, (args) => {
-			args = args || [];
+  handleEvent(data) {
+    this.handlers[data.action].call(this, data.args, (args) => {
+      args = args || [];
 
-			// Collect and send any changed properties...
-			let props = {},
-				transfers = [];
-			this.propList.forEach((propName) => {
-				let propVal = this.target[propName];
+      // Collect and send any changed properties...
+      let props = {},
+        transfers = [];
+      this.propList.forEach((propName) => {
+        let propVal = this.target[propName];
 
-				if (this.sentProps[propName] !== propVal) {
-					// Save this value for later reference...
-					this.sentProps[propName] = propVal;
+        if (this.sentProps[propName] !== propVal) {
+          // Save this value for later reference...
+          this.sentProps[propName] = propVal;
 
-					if (propName == 'duration' && isNaN(propVal) && isNaN(this.sentProps[propName])) {
-						// NaN is not === itself. Nice!
-						// no need to update it here.
-					} else if (propName == 'audioBuffer') {
-						// Buffers are already extracted from the heap.
-						// Don't copy; they are safe to transfer.
-						props[propName] = propVal;
-						if (propVal) {
-							for (let i = 0; i < propVal.length; i++) {
-								transfers.push(propVal[i].buffer);
-							}
-						}
-					} else if (propName == 'frameBuffer') {
-						// We already extract ahead of time now,
-						// so transfer the small buffers.
-						props[propName] = propVal;
-						if (propVal) {
-							transfers.push(propVal.y.bytes.buffer);
-							transfers.push(propVal.u.bytes.buffer);
-							transfers.push(propVal.v.bytes.buffer);
-						}
-					} else {
-						props[propName] = propVal;
-					}
-				}
-			});
+          if (
+            propName === "duration" &&
+            isNaN(propVal) &&
+            isNaN(this.sentProps[propName])
+          ) {
+            // NaN is not === itself. Nice!
+            // no need to update it here.
+          } else if (propName === "audioBuffer") {
+            // Buffers are already extracted from the heap.
+            // Don't copy; they are safe to transfer.
+            props[propName] = propVal;
+            if (propVal) {
+              for (let i = 0; i < propVal.length; i++) {
+                transfers.push(propVal[i].buffer);
+              }
+            }
+          } else if (propName === "frameBuffer") {
+            // We already extract ahead of time now,
+            // so transfer the small buffers.
+            props[propName] = propVal;
+            if (propVal) {
+              transfers.push(propVal.y.bytes.buffer);
+              transfers.push(propVal.u.bytes.buffer);
+              transfers.push(propVal.v.bytes.buffer);
+            }
+          } else {
+            props[propName] = propVal;
+          }
+        }
+      });
 
-			let out = {
-				action: 'callback',
-				callbackId: data.callbackId,
-				args: args,
-				props: props
-			};
-			if (this.transferables) {
-				postMessage(out, transfers);
-			} else {
-				postMessage(out);
-			}
-		});
-	}
+      let out = {
+        action: "callback",
+        callbackId: data.callbackId,
+        args: args,
+        props: props
+      };
+      if (this.transferables) {
+        postMessage(out, transfers);
+      } else {
+        postMessage(out);
+      }
+    });
+  }
 
-	workerOnMessage(event) {
-		let data = event.data;
-		if (!data || typeof data !== 'object') {
-			// invalid
-			return;
-		} else if (data.action == 'transferTest') {
-			// ignore
-		} else if (typeof data.action !== 'string' || typeof data.callbackId !== 'string' || typeof data.args !== 'object') {
-			console.log('invalid message data', data);
-		} else if (!(data.action in this.handlers)) {
-			console.log('invalid message action', data.action);
-		} else if (data.action == 'construct') {
-			// always handle constructor
-			this.handleEvent(data);
-		} else if (!this.target) {
-			// queue until constructed
-			this.pendingEvents.push(data);
-		} else {
-			this.handleEvent(data);
-		}
-	}
+  workerOnMessage(event) {
+    let data = event.data;
+    if (!data || typeof data !== "object") {
+      // invalid
+      return;
+    } else if (data.action === "transferTest") {
+      // ignore
+    } else if (
+      typeof data.action !== "string" ||
+      typeof data.callbackId !== "string" ||
+      typeof data.args !== "object"
+    ) {
+      console.log("invalid message data", data);
+    } else if (!(data.action in this.handlers)) {
+      console.log("invalid message action", data.action);
+    } else if (data.action === "construct") {
+      // always handle constructor
+      this.handleEvent(data);
+    } else if (!this.target) {
+      // queue until constructed
+      this.pendingEvents.push(data);
+    } else {
+      this.handleEvent(data);
+    }
+  }
 }
 
 export default ASWorkerSupport;
