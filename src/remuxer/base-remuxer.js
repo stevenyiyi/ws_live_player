4d63edfb83817db61bde66db5635aefa6414f61f
yi.cheng <104037806@qq.com>
2023-11-28 20:34:03 +0800

    dADdaD

--- src/remuxer/base-remuxer.js
+++ src/remuxer/base-remuxer.js
@@ -7,29 +7,6 @@ export class BaseRemuxer {
     return 90000;
   }
 
-  // TODO: move to ts parser
-  // static PTSNormalize(value, reference) {
-  //
-  //     let offset;
-  //     if (reference === undefined) {
-  //         return value;
-  //     }
-  //     if (reference < value) {
-  //         // - 2^33
-  //         offset = -8589934592;
-  //     } else {
-  //         // + 2^33
-  //         offset = 8589934592;
-  //     }
-  //     /* PTS is 33bit (from 0 to 2^33 -1)
-  //      if diff between value and reference is bigger than half of the amplitude (2^32) then it means that
-  //      PTS looping occured. fill the gap */
-  //     while (Math.abs(value - reference) > 4294967296) {
-  //         value += offset;
-  //     }
-  //     return value;
-  // }
-
   static getTrackID() {
     return track_id++;
   }
